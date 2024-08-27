0acf1605202f54dbd005979c0d9be7986e389b52
Bluetea-king <2779624283@qq.com>
2024-08-27 14:11:45 +0800

    add player

--- Assets/Player/h_player_control.cs
+++ Assets/Player/h_player_control.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class temp : MonoBehaviour
+public class h_player_control : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // greater
+        
     }
 
     // Update is called once per frame
