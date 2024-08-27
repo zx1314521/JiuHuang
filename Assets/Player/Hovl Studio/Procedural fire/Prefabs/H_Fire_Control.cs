c040345175c3aa575e796411619f052da6639d07
Bluetea-king <2779624283@qq.com>
2024-08-27 15:37:34 +0800

    player control update

--- Assets/Player/Hovl Studio/Procedural fire/Prefabs/H_Fire_Control.cs
+++ Assets/Player/Hovl Studio/Procedural fire/Prefabs/H_Fire_Control.cs
@@ -10,7 +10,7 @@ public class H_Fire_Control : MonoBehaviour
     void Start()
     {
         Player     = GameObject.FindWithTag("Player");
-        Move_dir   = Player.transform.forward; 
+        Move_dir   = Player.transform.forward.normalized;
     }
 
     float CD_Fire = 0;
@@ -19,11 +19,14 @@ public class H_Fire_Control : MonoBehaviour
         if(CD_Fire > 1)
         {
             GameObject effect = Instantiate(Bomb,transform.position,transform.rotation);
-            
             Destroy(gameObject);
             Destroy(effect, 2f);
         }
         CD_Fire += Time.deltaTime;
+
+        if (Move_dir.x < 0)  Move_dir.x = -1  * Move_dir.x;
+        if (Move_dir.y < 0)  Move_dir.y  = -1 * Move_dir.y;
+        if (Move_dir.z < 0)  Move_dir.z = -1  * Move_dir.z;
         transform.Translate(Move_dir * Time.deltaTime * 7);
     }
 }
--- Assets/Player/h_player_control.cs
+++ Assets/Player/h_player_control.cs
@@ -4,34 +4,33 @@ using UnityEngine;
 
 public class h_player_control : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject Fire;
-    void Start()
-    {
-        
-    }
+    public float speed = 5f; // Movement speed
+    public float rotationSpeed = 700f; // Rotation speed
 
-    // Update is called once per frame
     void Update()
     {
-        //获取虚拟轴
+        // 获取虚拟轴
         float horizontal = Input.GetAxis("Horizontal");
+        float vertical   = Input.GetAxis("Vertical");
 
-        //获取水平轴
-        float vertical = Input.GetAxis("Vertical");
+        // 构建向量
+        Vector3 dir = new Vector3(horizontal, 0, vertical).normalized;
 
-        //构建向量
-        Vector3 dir = new Vector3(horizontal, 0, vertical);
-
-        if(dir != Vector3.zero)
+        if (dir != Vector3.zero)
         {
-            transform.Translate(dir * Time.deltaTime);
+            // 移动角色
+            transform.Translate(dir * speed * Time.deltaTime, Space.World);
+
+            // 旋转角色
+            Quaternion toRotation = Quaternion.LookRotation(dir);
+            transform.rotation    = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
 
-        //按下鼠标左键发射子弹
+        // 按下鼠标左键发射子弹
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(Fire,transform.position,transform.rotation);
+            Instantiate(Fire, transform.position, transform.rotation);
         }
     }
 }
