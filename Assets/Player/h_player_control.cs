0d196b9ac003054cc127d019e6fa3c79a9d6b7d3
Bluetea-king <2779624283@qq.com>
2024-08-27 14:18:56 +0800

    Update h_player_control.cs

--- Assets/Player/h_player_control.cs
+++ Assets/Player/h_player_control.cs
@@ -13,6 +13,18 @@ public class h_player_control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //获取虚拟轴
+        float horizontal = Input.GetAxis("Horizontal");
+
+        //获取水平轴
+        float vertical = Input.GetAxis("Vertical");
+
+        //构建向量
+        Vector3 dir = new Vector3(horizontal, 0, vertical);
+
+        if(dir != Vector3.zero)
+        {
+            transform.Translate(dir * Time.deltaTime);
+        }
     }
 }
