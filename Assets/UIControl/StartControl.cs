5bb1cb7ae00cd2a72c2548491a93b5306db52511
zx1314521 <1249418456@qq.com>
2024-08-29 23:17:04 +0800

    UI

---
+++ Assets/UIControl/StartControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("PlayGame");
+    }
+}
